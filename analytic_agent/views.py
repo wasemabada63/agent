b537f549618318ea6574cf9fbe5a823343c5f64a
wasemabada63 <wasemabada63@gmail.com>
2026-04-24 14:53:19 +0300

    Update views.py

--- analytic_agent/views.py
+++ analytic_agent/views.py
@@ -1,12 +1,17 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from analytics_bot.src.pipeline import ask_retail_rag_ui
 
 @api_view(['POST'])
 def ask_retail_rag_ui(request):
-    from analytics_bot.src.pipeline import ask_retail_rag_ui
-    question = request.data.get("question", "")
-    out = ask_retail_rag_ui(question)
-    return Response(out)
+    if request.method == "GET":
+        return Response({"question": "What is the best selling product in the last month?"})
 
+    if request.method == "POST":
+        question = request.data.get("question", "")
+        out = ask_retail_rag_ui(question)
+        if type(out) == list:
+            return Response(out[len(out)-1])
+        else:
+            return Response(out)
 
-{    "question": "What is the best selling product in the last month?"}
