ca5c84be9e6be835bc65b21c662ec9b84c537db2
wasemabada63 <wasemabada63@gmail.com>
2026-04-25 14:18:02 +0300

    Update views.py

--- analytic_agent/views.py
+++ analytic_agent/views.py
@@ -2,19 +2,23 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from analytics_bot.src.pipeline import ask_retail_rag_ui
 
-@api_view(['POST','GET'])
+@api_view(['POST'])
 def ask_question(request):
     if request.method == "GET":
-        return Response({"question": "What is the top 5 sold products?"})
+        return Response({"question": "What is the best selling product in the last month?"})
 
     if request.method == "POST":
         question = request.data.get("question", "")
-        out = ask_retail_rag_ui(question)
-        print(type(out))
-        if isinstance(out,list):
-            return Response(out[len(out)-1])
-        else:
-            return Response(out)
+        try:
+            gen = ask_retail_rag_ui(question)
+            out_list = list(gen)
+            if out_list:
+                return Response(out_list[-1])
+            else:
+                return Response({"error": "out_list was empty", "type_of_gen": str(type(gen))})
+        except Exception as e:
+            import traceback
+            return Response({"error": str(e), "traceback": traceback.format_exc()})
     
     # {"question": "What is the best selling product in the last month?"}
 
